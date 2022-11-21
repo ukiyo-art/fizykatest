9c73295d59a070b9969da37be74a3f00d7f7267d
Bibaszek <123dawid6@gmail.com>
2022-11-21 17:28:17 +0100

    for githubpages update

--- server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-app.set('port', process.env.PORT || 8081)
-
-const server = app.listen(app.get('port'), () =>{
-    console.log(`Listening on port ${server.address().port}`);
-});
